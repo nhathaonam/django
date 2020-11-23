e4d945ef878f6c19659572a1596348ec39ddc660
nhathaonam <doanminhduong1793@gmail.com>
2020-11-23 16:25:07 +0700

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homePageView(request):
-    return HttpResponse('Hello, World')
+    return HttpResponse('Hello, This is page test')
